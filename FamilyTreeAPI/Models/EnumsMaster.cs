015e4686232deda28b06f8da07291621b686556b
amitdharmkamble <amit97dharmkamble>
2025-06-13 23:48:13 +0530

    chore : daily

--- FamilyTreeAPI/Models/EnumsMaster.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FamilyTreeAPI.Models
-{
-    public class EnumsMaster
-    {
-        public required string EnumName { get; set; }
-        public required int Number { get; set; }
-        public required string Text { get; set; }
-    }
-}
